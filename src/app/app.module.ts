8a091a6532004e9fc07f59b50479564f4afc12e7
Lorenzo Mancini <l.mancini@cineca.it>
2021-06-29 16:03:59 +0200

    aggiunta sottomodulo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { SharedModuleModule} from './shared-module/subModule/shared-module.module';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -10,6 +11,7 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
+    SharedModuleModule,
     AppRoutingModule
   ],
   providers: [],
